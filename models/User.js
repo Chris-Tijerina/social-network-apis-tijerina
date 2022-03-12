1e79d1af8f3925b2fb6a7ccfec5242804f15b54d
Christopher <ctijerina@live.com>
2022-03-12 16:13:38 -0600

    Created user model schema  with all required fields and data types

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+	{
+		username: {
+			type: String,
+			unique: true,
+			required: "Username is Required",
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: "Email is Required",
+			unique: true,
+			match: [/.+@.+\..+/],
+		},
+		thoughts: [
+			{
+				type: Schema.ObjectId,
+				ref: "thoughts",
+			},
+		],
+		friends: [
+			{
+				type: Schema.ObjectId,
+				ref: this.username,
+			},
+		],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+		},
+		id: false,
+	}
+);
+
+UserSchema.virtual("friendCount").get(function () {
+	return this.friends.length;
+});
+
+const user = model("User", UserSchema);
+
+module.exports = user;
