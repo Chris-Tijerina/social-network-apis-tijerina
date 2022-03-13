6851a6e6af01bbc6f8b5ee4092e8b69f5853d7fe
Christopher <ctijerina@live.com>
2022-03-13 16:56:49 -0500

    fixed syntax errors in thought model. now ready to test routes, excluding friend and reactions:

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,35 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
+const ReactionSchema = new Schema(
+	{
+		reactionId: {
+			type: Schema.Types.ObjectId,
+			default: () => new Types.ObjectId(),
+		},
+		reactionBody: {
+			type: String,
+			required: true,
+			maxlength: 280,
+		},
+		username: {
+			type: String,
+			required: true,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdAtVal) => dateFormat(createdAtVal),
+		},
+	},
+	{
+		toJSON: {
+			getters: true,
+		},
+		id: false,
+	}
+);
+
 const ThoughtSchema = new Schema(
 	{
 		thoughtText: {
@@ -30,38 +59,9 @@ const ThoughtSchema = new Schema(
 );
 
 // get total count of reactions on retrieval
-reactionCount.virtual("reactionCount").get(function () {
+ThoughtSchema.virtual("reactionCount").get(function () {
 	return this.reactions;
 });
-
-const ReactionSchema = new Schema(
-	{
-		reactionId: {
-			type: Schema.Types.ObjectId(),
-			default: () => new Types.ObjectId(),
-		},
-		reactionBody: {
-			type: String,
-			required: true,
-			maxlength: 280,
-		},
-		username: {
-			type: String,
-			required: true,
-		},
-		createdAt: {
-			type: Date,
-			default: Date.now,
-			get: (createdAtVal) => dateFormat(createdAtVal),
-		},
-	},
-	{
-		toJSON: {
-			getters: true,
-		},
-		id: false,
-	}
-);
 
 const Thought = model("Thought", ThoughtSchema);
 
