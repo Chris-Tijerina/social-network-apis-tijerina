d6b483307270f254dc23173d49b558a8ef503ee4
Christopher <ctijerina@live.com>
2022-03-13 17:51:45 -0500

    corrections made to thought controller, need to associate with the user. need to retest

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -13,7 +13,7 @@ const thoughtController = {
 
 	// get one thought by id
 	getThoughtById({ params }, res) {
-		Thought.findOne({ _id: params.id })
+		Thought.findOne({ _id: params.thoughtId })
 			.then((dbThoughtData) => {
 				// If no thought is found, send 404
 				if (!dbThoughtData) {
@@ -28,14 +28,27 @@ const thoughtController = {
 			});
 	},
 	// createThought
-	createThought({ body }, res) {
+	createThought({ params, body }, res) {
 		Thought.create(body)
-			.then((dbThoughtData) => res.json(dbThoughtData))
-			.catch((err) => res.status(400).json(err));
+			.then(({ _id }) => {
+				return User.findOneAndUpdate(
+					{ _id: params.userId },
+					{ $push: { thoughts: _id } },
+					{ new: true }
+				);
+			})
+			.then((dbUserData) => {
+				if (!dbUserData) {
+					res.status(404).json({ message: "No user found with this id!" });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch((err) => res.json(err));
 	},
 	// update Thought by id
 	updateThought({ params, body }, res) {
-		Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+		Thought.findOneAndUpdate({ _id: params.thoughtId }, body, { new: true })
 			.then((dbThoughtData) => {
 				if (!dbThoughtData) {
 					res.status(404).json({ message: "No thought found with this id!" });
@@ -47,15 +60,26 @@ const thoughtController = {
 	},
 	// delete Thought
 	deleteThought({ params }, res) {
-		Thought.findOneAndDelete({ _id: params.id })
+		Thought.findOneAndDelete({ _id: params.thoughtId })
 			.then((dbThoughtData) => {
 				if (!dbThoughtData) {
 					res.status(404).json({ message: "No thought found with this id!" });
 					return;
 				}
-				res.json(dbThoughtData);
+				return User.findOneAndUpdate(
+					{ _id: params.userId },
+					{ $pull: { thoughts: params.thoughtId } },
+					{ new: true }
+				);
 			})
-			.catch((err) => res.status(400).json(err));
+			.then((dbUserData) => {
+				if (!dbUserData) {
+					res.status(404).json({ message: "No user found with this id!" });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch((err) => res.json(err));
 	},
 };
 
