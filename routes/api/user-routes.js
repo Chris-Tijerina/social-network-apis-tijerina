0440ab32be8f1779b0af70f7dbfaefd2ca5b4948
Christopher <ctijerina@live.com>
2022-03-13 18:26:50 -0500

    added in the friend routes, time to test

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,6 +5,8 @@ const {
 	createUser,
 	updateUser,
 	deleteUser,
+	addFriend,
+	deleteFriend,
 } = require("../../controllers/user-controller");
 
 // Set up GET all and POST at /api/users
@@ -12,5 +14,11 @@ router.route("/").get(getAllUser).post(createUser);
 
 // Set up GET one, PUT, and DELETE at /api/users/:id
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// Set up POST and DELETE at /api/users/:userId/friends/:friendsId
+router
+	.route("/api/users/:userId/friends/:friendsId")
+	.post(addFriend)
+	.delete(deleteFriend);
 
 module.exports = router;
