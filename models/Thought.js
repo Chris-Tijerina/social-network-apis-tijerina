1bd4f921edcd3e887691b747b100e58fa95f7c02
Christopher <ctijerina@live.com>
2022-03-13 16:19:33 -0500

    added thought schema with virtual for reaction count

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,35 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ThoughtSchema = new Schema(
+	{
+		thoughtText: {
+			type: String,
+			required: true,
+			minlength: 1,
+			maxlength: 280,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdAtVal) => dateFormat(createdAtVal),
+		},
+		username: {
+			type: String,
+			required: true,
+		},
+		reactions: [ReactionSchema],
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true,
+		},
+		id: false,
+	}
+);
+
+// get total count of reactions on retrieval
+reactionCount.virtual("reactionCount").get(function () {
+	return this.reactions;
+});
